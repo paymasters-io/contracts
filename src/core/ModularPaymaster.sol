7ebe3c37655c401f336f2857b93b2e58a5854d8b
peteruche21 <anyaogupeter601@gmail.com>
2023-08-25 00:57:26 +0100

    feat: abstract base module

--- src/core/ModularPaymaster.sol
+++ src/core/ModularPaymaster.sol
@@ -1,6 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract ModularPaymaster {
-    constructor() {}
+// eth-infinitsm
+import "@aa/contracts/core/BasePaymaster.sol";
+import "@aa/contracts/core/Helpers.sol";
+import "@aa/contracts/interfaces/UserOperation.sol";
+import "@aa/contracts/interfaces/IEntryPoint.sol";
+
+contract ModularPaymaster is BasePaymaster {
+    constructor(IEntryPoint _entryPoint, address _owner) BasePaymaster(_entryPoint) Ownable(_owner) {}
+
+    function _validatePaymasterUserOp(
+        UserOperation calldata userOp,
+        bytes32,
+        uint256 requiredPreFund
+    ) internal override returns (bytes memory context, uint256 validationResult) {
+        unchecked {
+            validationResult = 1;
+        }
+    }
+
+    function _postOp(PostOpMode mode, bytes calldata context, uint256 actualGasCost) internal override {
+        unchecked {}
+    }
 }
--- src/modules/BaseModule.sol
+++ src/modules/BaseModule.sol
@@ -1,2 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
+
+abstract contract BaseModule {
+    address public immutable paymaster;
+
+    error NotAuthorized(address sender);
+
+    constructor(address _paymaster, address _vaa, bool _requiresVaaSig) {
+        // _paymaster.registerModule(_vaa, _requiresVaaSig);
+        paymaster = _paymaster;
+    }
+
+    modifier onlyPaymastersIo() {
+        if (msg.sender != paymaster) {
+            revert NotAuthorized(msg.sender);
+        }
+        _;
+    }
+
+    // funds the verifying paymaster
+    function stake() external payable virtual;
+
+    // defunds the verifying paymaster
+    function unstake(uint256 amount) external virtual;
+
+    // withdraws the balances from this module
+    function withdraw() external virtual;
+
+    // returns the stake of the user
+    function getStake(address user) external view virtual returns (uint256);
+
+    // used to validate the user operation.
+    // called by paymaster.io
+    function validate(bytes calldata paymasterAndData, address user) external view virtual returns (bool);
+
+    // post operation hook
+    // called by paymaster.io
+    function postValidate(bytes calldata context, uint256 actualGasCost, address user) external virtual;
+
+    // internal function that must be overidden by the inheriting module
+    function _validate(bytes calldata paymasterAndData, address user) internal view virtual returns (bool);
+
+    // internal function that must be overidden by the inheriting module
+    function _postValidate(bytes calldata context, uint256 actualGasCost, address user) internal virtual;
+
+    // fallback to receive ether
+    receive() external payable virtual;
+}

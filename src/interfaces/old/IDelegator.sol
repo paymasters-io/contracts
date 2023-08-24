6c82a6c69b323d9ff5a0b39e519f1db8c5eee568
peteruche21 <anyaogupeter601@gmail.com>
2023-08-24 21:14:41 +0100

    feat: deleting old files

--- src/interfaces/old/IDelegator.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
-
-interface IDelegator {
-    /// paymasterAndData[4:24] : address(delegator)  || address(this) 20 byte
-    /// paymasterAndData[24:56] : bytes32(hash) 32byte
-    /// paymasterAndData[56:] : bytes(signatures) >64byte
-    function previewValidation(bytes calldata paymasterAndData, address caller) external view returns (bool);
-}
--- src/interfaces/old/IRebate.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
-
-interface IRebate {
-    function rebate(address caller, uint256 value, uint256 refundValue, bytes calldata _context) external;
-}
